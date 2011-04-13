f22520b1896e2e0e0753dde1ea65d75b9388fa38
Yongseok Kang <feelform@gmail.com>
2011-04-13 11:09:17 +0900

    create project file. create MainViewController using TableViewController.

---
+++ PhotoTranslator/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  PhotoTranslator
+//
+//  Created by Yongseok Kang on 4/13/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MainViewController : UITableViewController {
+    
+}
++ (MainViewController *)controller;
+@end
